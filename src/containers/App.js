746b4334eedb1e1249a195c201b42b9b278f6fb3
Teodor Ignat <teodorignat96@gmail.com>
2023-10-08 00:09:06 +0100

    Search bar previous state problem solved

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,6 +15,7 @@ class App extends Component {
       jokesCategory: [],
       catValue: 'default',
       searchJokes: false,
+      newQueryJokes: [],
       queryJokes: [],
       searchfield: '',
       results: false,
@@ -70,45 +71,40 @@ class App extends Component {
 
     fetchSearchJokes = () => {
       try {
-        fetch(`https://api.chucknorris.io/jokes/search?query=${this.state.searchfield}`)
+        return fetch(`https://api.chucknorris.io/jokes/search?query=${this.state.searchfield}`)
           .then(response => response.json())
-          .then(searchJoke => this.setState({searchJokes: searchJoke}))
+          .then(searchJoke => {
+            this.setState({searchJokes: searchJoke});
+            return searchJoke;
+        })
       } catch (error) {
         alert('Jokes are not available at the moment! Please try again later.');
       }
     }
 
     handleSearchButton = () => {
-      const { searchfield, searchJokes, countResult, queryJokes} = this.state;
-      try {
-        if (searchfield.length > 0) {
-          this.fetchSearchJokes();
+      const { searchfield, countResult, queryJokes} = this.state;
+    
+      if (searchfield.length > 0) {
+        this.fetchSearchJokes().then(searchJokes => {
           if (searchJokes && searchJokes.total > 0) {
-            setTimeout(() => {
-              searchJokes.result.map(queryjoke => {
-                return queryJokes.push(queryjoke)
-              })
-              console.log('query', queryJokes);
-              this.setState({jokes: queryJokes[Math.floor(Math.random() * searchJokes.total)]});
-              this.setState({queryJokes: []});
-              this.setState({countResult: countResult + 1});
-            }, 200)
-          } else if (searchJokes.total === 0 ) {
-            this.setState({jokes: { value: 'There are no jokes with your word/s included.'}});
+            const newQueryJokes = [...queryJokes, ...searchJokes.result];
+            this.setState({
+              jokes: newQueryJokes[Math.floor(Math.random() * searchJokes.total)],
+              queryJokes: [],
+              countResult: countResult + 1
+            });
+          } else if (searchJokes.total === 0) {
+            this.setState({ jokes: { value: 'There are no jokes with your word/s included.' } });
           } else {
-            this.setState({jokes: { value: 'Press the button one more time, Chuck Norris gets the answer first.'}});
-            
+            this.setState({ jokes: { value: 'Press the button one more time, Chuck Norris gets the answer first.' } });
           }
-        }
-        
-        this.setState({nextJoke: !this.state.nextJoke})
-        setTimeout(() => {
-          return this.setState({nextJoke: false});
-        }, 1000)
-        
-      } catch (error) {
-        alert(`Chuck Norris is BUSY right now! Try again!`);
-        console.error(error);
+    
+          this.setState({ nextJoke: !this.state.nextJoke });
+          setTimeout(() => {
+            return this.setState({ nextJoke: false });
+          }, 1000);
+        });
       }
     }
 
